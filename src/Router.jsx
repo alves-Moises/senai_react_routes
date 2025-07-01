9e810a256541a3cffb05bc95fdb01db6feddc425
alves-Moises <moisesinho_ra@hotmail.com>
2025-07-01 01:43:26 -0300

    Update routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import  { Home }   from "./pages/Home"
+import  { Home }    from "./pages/Home"
 import  { About }   from "./pages/About"
+import { Mauricio } from "./pages/Mauricio"
+
 
 export function Router(){
     return(
         <Routes>
             <Route path="/"  element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/mauricio" element={<Mauricio />}/>
         </Routes>
     )
 }
